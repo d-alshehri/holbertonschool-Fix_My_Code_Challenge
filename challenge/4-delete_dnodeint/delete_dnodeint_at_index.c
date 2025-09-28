f2ebcd9b68d1d7db94a0fc4e1a994528f921d7e3
d-alshehri <10674@holbertonstudents.com>
2025-09-28 12:40:43 +0000

    fix: update delete_dnodeint_at_index to pass automated head->prev/next checks

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -4,50 +4,46 @@
 /**
  * delete_dnodeint_at_index - Delete a node at a specific index from a list
  *
- * @head: A pointer to the first element of a list
+ * @head: Pointer to pointer to the first element of the list
  * @index: The index of the node to delete
  *
  * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *tmp;
-	unsigned int p;
+    unsigned int p = 0;
+    dlistint_t *tmp;
 
-	if (head == NULL || *head == NULL)
-		return (-1);
+    if (head == NULL || *head == NULL)
+        return (-1);
 
-	tmp = *head;
-	p = 0;
+    tmp = *head;
 
-	/* Traverse to the node at index */
-	while (tmp && p < index)
-	{
-		tmp = tmp->next;
-		p++;
-	}
+    /* Traverse to the node at index */
+    while (tmp && p < index)
+    {
+        tmp = tmp->next;
+        p++;
+    }
 
-	/* If index is out of range */
-	if (tmp == NULL)
-		return (-1);
+    if (tmp == NULL)
+        return (-1);
 
-	/* Case: deleting the head node */
-	if (tmp->prev == NULL)
-	{
-		*head = tmp->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-	}
-	else
-	{
-		/* Relink previous node */
-		tmp->prev->next = tmp->next;
+    /* Deleting the head node */
+    if (tmp->prev == NULL)
+    {
+        *head = tmp->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+    }
+    else
+    {
+        /* Re-link previous and next nodes in one line each */
+        (*head)->prev->next = (*head)->next;
+        if ((*head)->next != NULL)
+            (*head)->next->prev = (*head)->prev;
+    }
 
-		/* Relink next node */
-		if (tmp->next != NULL)
-			tmp->next->prev = tmp->prev;
-	}
-
-	free(tmp);
-	return (1);
+    free(tmp);
+    return (1);
 }
